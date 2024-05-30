95db5101f292c0f3dfe61d29f29a9794891edc45
adsyoga <107547179+adsyoga@users.noreply.github.com>
2024-05-30 21:23:33 +0700

    Add files via upload

---
+++ models/bid_request.go
@@ -0,0 +1,50 @@
+package models
+
+import (
+	"encoding/json"
+	"time"
+
+	"github.com/gobuffalo/pop/v6"
+	"github.com/gobuffalo/validate/v3"
+	"github.com/gofrs/uuid"
+)
+
+// BidRequest is used by pop to map your bid_requests database table to your go code.
+type BidRequest struct {
+	ID        uuid.UUID `json:"id" db:"id"`
+	CreatedAt time.Time `json:"created_at" db:"created_at"`
+	UpdatedAt time.Time `json:"updated_at" db:"updated_at"`
+}
+
+// String is not required by pop and may be deleted
+func (b BidRequest) String() string {
+	jb, _ := json.Marshal(b)
+	return string(jb)
+}
+
+// BidRequests is not required by pop and may be deleted
+type BidRequests []BidRequest
+
+// String is not required by pop and may be deleted
+func (b BidRequests) String() string {
+	jb, _ := json.Marshal(b)
+	return string(jb)
+}
+
+// Validate gets run every time you call a "pop.Validate*" (pop.ValidateAndSave, pop.ValidateAndCreate, pop.ValidateAndUpdate) method.
+// This method is not required and may be deleted.
+func (b *BidRequest) Validate(tx *pop.Connection) (*validate.Errors, error) {
+	return validate.NewErrors(), nil
+}
+
+// ValidateCreate gets run every time you call "pop.ValidateAndCreate" method.
+// This method is not required and may be deleted.
+func (b *BidRequest) ValidateCreate(tx *pop.Connection) (*validate.Errors, error) {
+	return validate.NewErrors(), nil
+}
+
+// ValidateUpdate gets run every time you call "pop.ValidateAndUpdate" method.
+// This method is not required and may be deleted.
+func (b *BidRequest) ValidateUpdate(tx *pop.Connection) (*validate.Errors, error) {
+	return validate.NewErrors(), nil
+}
